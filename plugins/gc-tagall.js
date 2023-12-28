43194ed550f8d278dbd09571e441a5a20ce24103
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2023-12-28 21:44:51 +0100

    👻 𝗙𝗔𝗡𝗧𝗔𝗦𝗠𝗜𝗡 𝗕𝗢𝗧 👻

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,12 +5,12 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `🕷️ 𝗦𝗽𝗶𝗱𝗲𝗿 𝗕𝗼𝘁 / @𝗦𝗽𝗶𝗱𝗲𝗿.𝗩𝗲𝗻𝘁𝗮𝘀.𝗕𝗼𝘁 🕷️ ${pesan}\n`;
-  let teks = `𝗦𝗽𝗶𝗱𝗲𝗿 𝗕𝗼𝘁 𝗧𝗲 𝗜𝗻𝘃𝗼𝗰𝗮 𝗣𝗹𝗮𝗻𝘁𝗶𝘁𝗮 𝗔𝗰𝘁𝗶𝘃𝗮𝘁𝗲 𝗚𝗼𝗼 🕷️\n\n${oi}\n`;
+  const oi = `👻 𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶𝗻 𝗕𝗼𝘁 / @𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶𝗻.𝗩𝗲𝗻𝘁𝗮𝘀.𝗕𝗼𝘁 👻 ${pesan}\n`;
+  let teks = `𝗙𝗮𝗻𝘁𝗮𝘀𝗺𝗶𝗻  𝗧𝗲 𝗜𝗻𝘃𝗼𝗰𝗮 𝗣𝗹𝗮𝗻𝘁𝗶𝘁𝗮 𝗔𝗰𝘁𝗶𝘃𝗮𝘁𝗲 𝗚𝗼𝗼 👻\n\n${oi}\n`;
   for (const mem of participants) {
-    teks += `ִ໋🕷️ ➢ @${mem.id.split('@')[0]}\n`;
+    teks += `ִ໋👻 ➢ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `\n 🕷️ 𝗦𝗣𝗜𝗗𝗘𝗥 𝗕𝗢𝗧 🕷️`;
+  teks += `\n 👻 𝗙𝗔𝗡𝗧𝗔𝗦𝗠𝗜𝗡 𝗕𝗢𝗧 👻`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
