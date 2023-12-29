6b05fffd65fb12cfb20ede61a64db39c01bcf144
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2023-12-29 20:48:07 +0100

    👻 𝗙𝗔𝗡𝗧𝗔𝗦𝗠𝗜𝗡 𝗕𝗢𝗧 👻

--- plugins/rpg-cofre.js
+++ plugins/rpg-cofre.js
@@ -19,7 +19,7 @@ const handler = async (m, {isPrems, conn}) => {
 ║┈┈┈┈┈┈┈┈┈┈┈┈┈
 ║➢ *${dia} Diamantes* 💎
 ║➢ *${tok} Tokens* 🪙
-║➢ *${mystic} SpiderCoins* 👾
+║➢ *${mystic} ºFantasminCoins* 👾
 ║➢ *${expp} Exp* ⚡
 ╚═════════════════⬣`;
 
