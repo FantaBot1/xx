0a3a06435c792b8b84b78b17f56b4e304fdcfe9f
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2023-12-30 05:03:36 +0100

    👻 𝗙𝗔𝗡𝗧𝗔𝗦𝗠𝗜𝗡 𝗕𝗢𝗧 👻
    👻 𝗙𝗔𝗡𝗧𝗔𝗦𝗠𝗜𝗡 𝗕𝗢𝗧 👻

---
+++ plugins/gc-apos.js
@@ -0,0 +1,29 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.apos);
+};
+handler.command = /^(Apos|Apostado)$/i;
+export default handler;
+
+global.apos = `
+*☠ TOP HACKERS ☠*
+
+*➤ REGLAS APOSTADO*
+*HABILIDAD A. :* _ Alok_
+*HABILIDADES P. :*  _Maxim , Kelly y Moco_
+*Armas :*  _Ump , 1 M1014 Por Equipo Y Desert_
+
+*ACLARACIONES :*
+_La 1ra Ronda Solo Vale Desert_
+_Altura Que Si Vale : Cajas , Autos y Contenedores ( Observatorio Y Mill No Cuentan Como Altura )_
+_Si Ambas Escuadras Son Movil El Emulador Se Bloquea_
+_Para 2da Sala Tiene Que Quedar En 7-5 Si Queda En 7-4 Gana Equipo Contrario_
+
+*Creacion De Sala ☠*
+_1ra crea :rival_
+_2da crea: Top Hackers_
+_Aditamentos :  no_
+_Propiedad de armas : no_
+_Sala Avanzada: desert, ump, m10_
+_Aidrop/Cibernetico : no_
+_1 PC máximo_
+`;
