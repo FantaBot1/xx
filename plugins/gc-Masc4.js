44072fe6b1ba2253dabefcf28cccd7b589c8592f
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-01-05 10:09:34 +0100

    q

--- plugins/gc-Masc4.js
+++ plugins/gc-Masc4.js
@@ -1,13 +1,13 @@
 const handler = async (m, {conn}) => {
-  m.reply(global.Masc4);
+  m.reply(global.Mixto4);
 };
-handler.command = /^(Masc4|masc4|liga)$/i;
+handler.command = /^(Mixto4|Mixto4|Mixto4)$/i;
 export default handler;
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
 
-global.Masc4 = `
+global.Mixto4 = `
 *VERSUS MIXTO*
 
 *HORARIO*
