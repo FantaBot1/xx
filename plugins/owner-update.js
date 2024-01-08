1eb48d0354df27a31a76e0ba3e177270f63db4e0
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-01-08 22:52:21 +0100

    〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -5,7 +5,7 @@ const handler = async (m, { conn, text }) => {
           const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
           let messager = stdout.toString()
           if (messager.includes('Already up to date.')) messager = '*[❗] No hay nada por actualizar, todo esta actualizado a como esta en el repositorio oficial.*'
-          if (messager.includes('Updating')) messager = '🕷️ 𝗦𝗣𝗜𝗗𝗘𝗥 𝗕𝗢𝗧🕷️\n\n' + stdout.toString()
+          if (messager.includes('Updating')) messager = '〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕\n\n' + stdout.toString()
           conn.reply(m.chat, messager, m);
   } catch {      
  try {    
